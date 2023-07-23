ac8ff1c2e5f3e10f0dcc57fa1bc63fe933d831fe
Lapo9 <lapofalcone@gmail.com>
2023-07-23 03:49:28 +0200

    Commented code

--- LootBoxSmartContract/LootBoxClient.py
+++ LootBoxSmartContract/LootBoxClient.py
@@ -8,25 +8,38 @@ from starknet_py.contract import Contract
 import PySimpleGUI as gui
 import webbrowser
 
+#----------------------------------------------------------------------------------------------------------------------------------------
+#SMART CONTRACT INTERACTION PART
+
+#constants holding the identifiers for events
 TOKENS_SENT = 0x26c7b52e2b50adbd331df67ab21e2c6448783a7b442ac0a0b7afaae2ef88ce
 LOOTBOX_BOUGHT = 0x3e98c04afc3e9e5e8d2ce01282731f4886c1ba831261c580d0947c3e0319d9a
 NOT_ENOUGH_TOKENS = 0x18b7f21fbc0b80823453c69654ad2c81e059000ef43a6f87b354b45a5ebc279
 
+#address of the contract we use
+CONTRACT_ADDRESS = 0x070356a6f62cfe0cdcdb0fd9a7e95baefca1a88bef10612b7a5d50fd6abe2343
+CONTRACT_ADDRESS_2 = 0x00b3d0e59cfc4572ca4542713907d7aae292904bed1b39ee570b57bf6cfd27f1
+
+#address of the player (me in this case)
+PLAYER_ADDRESS = 0x04415e87ae0acd543f309ac5381881fa254ae6eba6ff47aaf23a3e3894cc9991
+
+#object to interact with Starknet
 node = FullNodeClient(node_url="https://starknet-goerli.infura.io/v3/dda8ef6575244eb7a7214cca1b4c822c")
 
+#abstraction of a Starknet account
 account = Account(
     client=node,
-    address="0x04415e87ae0acd543f309ac5381881fa254ae6eba6ff47aaf23a3e3894cc9991",
+    address=hex(PLAYER_ADDRESS),
     key_pair=KeyPair(private_key=0x4c8f535679a167b3b25eed4cccfea27c0368e4765a26d9c1cf1e41ab20f5387, public_key=0x549d8254c94993bbd90d0fcb78f3e9a48dba2e9987b1fd4bd4fc67957b8e75e),
     chain=StarknetChainId.TESTNET,
 )
 
-contractAddress = 0x070356a6f62cfe0cdcdb0fd9a7e95baefca1a88bef10612b7a5d50fd6abe2343
-contract = Contract.from_address_sync(address=contractAddress, provider=account)
+#abstraction of a Starknet contract
+contract = Contract.from_address_sync(address=CONTRACT_ADDRESS, provider=account)
 
-playerAddress = 0x04415e87ae0acd543f309ac5381881fa254ae6eba6ff47aaf23a3e3894cc9991
 
-#buys a loot box and returns whether the transaction succeded or not, and the transaction hash
+
+#buys a loot box and returns whether the transaction succeded or not, the transaction hash and the data of the event generated
 def buyLootBox():
     invocation = contract.functions["buyLootBox"].invoke_sync(max_fee=int(1e15))
     
@@ -42,13 +55,14 @@ def buyLootBox():
     
     #success
     if (event.keys[0] == LOOTBOX_BOUGHT):
-        lootboxResult = (event.data[1], event.data[2], event.data[4])
+        lootboxResult = (event.data[1], event.data[2], event.data[4]) #(currency[0=gems, 1=gold], amount, remaining tokens)
         return (True, hex(invocation.hash), lootboxResult)
     #failure
     else:
         return (False, hex(invocation.hash), (0,0,0))
 
 
+#sends the specified amount of tokens to the specified address. Returns whether the transaction succeed, the hash of the transaction and the generated event data
 def sendTokens(address, amount):
     invocation = contract.functions["sendTokens"].invoke_sync(address, amount, max_fee=int(1e15))
     
@@ -64,24 +78,26 @@ def sendTokens(address, amount):
     
     #success
     if (event.keys[0] == TOKENS_SENT):
-        transferResult = (event.data[0], event.data[1], event.data[2])
+        transferResult = (event.data[0], event.data[1], event.data[2]) #(sender address, receiver address, remaining tokens)
         return (True, hex(invocation.hash), transferResult)
     #failure
     else:
         return (False, hex(invocation.hash), (0,0,0))
 
 
+#returns (tokens, gems, gold)
 async def getPlayerAssets(address):
-    return await contract.functions["getUserAssets"].call(address)
+    return (await contract.functions["getUserAssets"].call(address))[0]
 
-
+#returns the price to buy a loot box
 async def getLootBoxPrice():
     return await contract.functions["getLootboxPrice"].call()
 
 
+#----------------------------------------------------------------------------------------------------------------------------------------
+#GUI PART
 
-
-
+#a style to use in the GUI
 font = ("Courier New", 16)
 
 gui.theme("DarkBlue")
@@ -97,9 +113,9 @@ sendLayout =    [
                     [gui.Button("Send tokens", font=font, key="SEND"), gui.Image("./images/hourglass.png", visible=False, key="SENDING", size=(50,50))]
                 ]
 assetsLayout =  [
-                    [gui.Image("./images/token.png", key="TOKEN_ICON", size=(50,50)), gui.Text("0", font=font, key="TOKENS")],
-                    [gui.Image("./images/gold.png", key="GOLD_ICON", size=(50,50)), gui.Text("0", font=font, key="GOLD")],
-                    [gui.Image("./images/gem.png", key="GEM_ICON", size=(50,50)), gui.Text("0", font=font, key="GEMS")],
+                    [gui.Image("./images/token.png", key="TOKEN_ICON", size=(50,50)), gui.Text("", font=font, key="TOKENS")],
+                    [gui.Image("./images/gold.png", key="GOLD_ICON", size=(50,50)), gui.Text("", font=font, key="GOLD")],
+                    [gui.Image("./images/gem.png", key="GEM_ICON", size=(50,50)), gui.Text("", font=font, key="GEMS")],
                 ]
 boxLayout =     [
                     [gui.Text("Last box", font=("Courier New", 20, "underline"))],
@@ -121,25 +137,37 @@ layout =    [
             ]
 window = gui.Window(title="Loot boxes", layout=layout, margins=(100,50))
 
+
 async def main():
-    window.read()
-    await updateAssets()
-    
+    window.read() #initialize window
     lastTransaction = 0x0 #stores the last transaction hash, in order to show it when "View last transaction" is clicked
+    await updateAssets() #set the value for the assets
+    
+    #event loop
     while not window.was_closed():
         event, values = window.read()
+
+        #view contract button clicked
         if (event == "VIEW_CONTRACT"):
-            webbrowser.open("https://testnet.starkscan.co/contract/" + hex(contractAddress)) #open starkscan page
+            webbrowser.open("https://testnet.starkscan.co/contract/" + hex(CONTRACT_ADDRESS)) #open starkscan page
+        
+        #view transaction button clicked
         elif (event == "VIEW_TRANSACTION"):
             webbrowser.open("https://testnet.starkscan.co/tx/" + lastTransaction) #open starkscan page
+        
+        #send tokens button clicked
         elif (event == "SEND"):
             window["SENDING"].update(visible=True) #show hourglass
             window["SEND"].update(disabled=True) #disable button until the transaction is complete
             window.perform_long_operation(lambda: sendTokens(int(values["RECEIVER"], 16), int(values["AMOUNT"], 10)), "TOKEN_SENT") #initiate token sending operation (will cast a TOKEN_SENT event when done)
+        
+        #open lootbox button clicked
         elif (event == "OPEN"): #initiate box opening operation (will cast a LOOTBOX_BOUGHT event when done)
             window["OPENING"].update(visible=True) #show hourglass
             window["OPEN"].update(disabled=True) #disable button until the transaction is complete
             window.perform_long_operation(lambda: buyLootBox(), "LOOTBOX_BOUGHT")
+        
+        #the Starknet (blocking) operation of sending tokens finished
         elif (event == "TOKEN_SENT"):
             window["SENDING"].update(visible=False) #hide hourglass
             success, transaction, transferResult = values["TOKEN_SENT"] #get return value
@@ -148,8 +176,10 @@ async def main():
                 _, _, remainingTokens = transferResult
                 window["TOKENS"].update(remainingTokens) #update the text of the tokens (we avoid calling updateAssets since we already have all the values)
             window["SEND"].update(disabled=False) #re-enable button
+        
+        #the Starknet (blocking) operation of opening a lootbox finished
         elif (event == "LOOTBOX_BOUGHT"):
-            window["OPENING"].update(visible=False) #show hourglass
+            window["OPENING"].update(visible=False) #hide hourglass
             success, transaction, lootboxContent = values["LOOTBOX_BOUGHT"] #get return value
             lastTransaction = transaction #update last transaction
             if (success):
@@ -160,14 +190,13 @@ async def main():
                     window["BOX_CONTENT_IMG"].update(filename="./images/gem.png")
                 elif (currency == 1):
                     window["BOX_CONTENT_IMG"].update(filename="./images/gold.png")
-                await updateAssets() #update player's assets text
+                await updateAssets() #update player's assets texts
             window["OPEN"].update(disabled=False) #re-enable button
 
 
-
-#update text based on received assets
+#pulls user assets data from Starknet and updates texts based on received assets
 async def updateAssets():
-    tokens, gems, gold = (await getPlayerAssets(playerAddress))[0] #get the assets of the player
+    tokens, gems, gold = (await getPlayerAssets(PLAYER_ADDRESS)) #get the assets of the player
     window["TOKENS"].update(tokens)
     window["GOLD"].update(gold)
     window["GEMS"].update(gems)
--- LootBoxSmartContract/LootBoxSmartContract.cairo
+++ LootBoxSmartContract/LootBoxSmartContract.cairo
@@ -1,3 +1,6 @@
+//This Starknet smart contract simulates loot boxes in videogames.
+//Since it is immutable and public, it guarantees that the drop ratios specified by a software house are respected.
+
 #[contract]
 mod LootBox {
     use starknet::get_execution_info;
@@ -6,6 +9,7 @@ mod LootBox {
     use box::BoxTrait;
     use option::OptionTrait;
 
+    //the storage simply contains mappings associating the user and the amount of each assets they have
     struct Storage {
         usersTokens: LegacyMap<ContractAddress, u256>,
         usersGems: LegacyMap<ContractAddress, u256>,
@@ -17,22 +21,22 @@ mod LootBox {
         usersTokens::write(masterAddress, 1000000); //make the owner rich
     }
 
-    #[event] //signal loot box opening (currency: 0 = gems, 1 = gold)
+    #[event] //signals loot box opening (currency: 0 = gems, 1 = gold)
     fn lootBoxResult(user: ContractAddress, currency: u8, amount: u256, remainingTokens: u256){}
 
-    #[event] //signal error
+    #[event] //signals error
     fn notEnoughTokens(user: ContractAddress, remainingTokens: u256){}
 
     #[event] //tokens sent
     fn tokensSent(sender: ContractAddress, receiver: ContractAddress, remainingTokens: u256){}
     
 
-    #[external]
+    #[external] //extracts a random item and adds it to the player assets
     fn buyLootBox() {
         let userAddress = get_execution_info().unbox().caller_address; //get user address
         let availableTokens = usersTokens::read((userAddress)); //extract how many tokens the user has
 
-        //execute contract only if the user has enough tokens (assert is very buggy on this version of Starknet, it would be a better option)
+        //executes contract only if the user has enough tokens (assert is very buggy on this version of Starknet, it would be a better option)
         if availableTokens > 0 {
             //pay 1 token
             usersTokens::write(userAddress, usersTokens::read((userAddress)) - 1);
@@ -40,9 +44,9 @@ mod LootBox {
             //here we extract a pseudo random number, based on the hash of the incoming transaction
             let transactionHash = get_execution_info().unbox().tx_info.unbox().transaction_hash; //get the hash of the transaction
             let transactionHash256: u256 = transactionHash.into(); //cast the hash to a 256 integer
-            let random = transactionHash256 & 0xfff; //keep only the last 2 digits (range from 0 to 256)
+            let random = transactionHash256 & 0xfff; //keep only the last 3 digits (range from 0 to 4096)
             
-            //25% chance of getting gems
+            //25% chance of getting gems (if the last hex digit (0-15) is less than 4
             if (random & 0xf) < 4 {
                 usersGems::write(userAddress, usersGems::read((userAddress)) + random); //add gems to account
                 lootBoxResult(userAddress, 0, random, availableTokens-1);  //signal event
@@ -53,25 +57,26 @@ mod LootBox {
             }
         }
         else {
-            notEnoughTokens(userAddress, availableTokens); //error
+            notEnoughTokens(userAddress, availableTokens); //signal error (user doesn't have enough tokens)
         }
     }
 
-    #[external]
+    #[external] //sends the specified amount of tokens to the specified account
     fn sendTokens(receiverAddress: ContractAddress, amount: u256) {
         let userAddress = get_execution_info().unbox().caller_address; //get user address
         let availableTokens = usersTokens::read((userAddress)); //extract how many tokens the user has
+        
         if availableTokens < amount {
-            notEnoughTokens(userAddress, availableTokens);
+            notEnoughTokens(userAddress, availableTokens); //signal error (user wants to send more tokens than what he has)
         }
         else {
             usersTokens::write(userAddress, availableTokens - amount); //remove tokens from user
             usersTokens::write(receiverAddress, usersTokens::read((receiverAddress)) + amount); //add tokens to the receiver
-            tokensSent(userAddress, receiverAddress, availableTokens - amount);
+            tokensSent(userAddress, receiverAddress, availableTokens - amount); //signal transfer
         }
     }
 
-    #[view]
+    #[view] //returns the assets of the specified player
     fn getUserAssets(userAddress: ContractAddress) -> (u256, u256, u256) {
         let tokens = usersTokens::read((userAddress));
         let gems = usersGems::read((userAddress));
@@ -79,7 +84,7 @@ mod LootBox {
         return (tokens, gems, gold);
     }
 
-    #[view]
+    #[view] //returns how many tokens a loot box costs
     fn getLootBoxPrice() -> u256 {
         1
     }
